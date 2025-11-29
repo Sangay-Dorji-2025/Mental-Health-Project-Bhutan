c0413d8a7c5d21a651fb7525992eb1ba84162fd7
Sangay Dorji <sdorji753@gmail.com>
2025-11-29 15:36:47 +0600

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,8 @@ st.write("This section contains a minimal cleaning workflow. Modify it as needed
 #df_clean = df.isnull().sum()
 #df_clean.columns.values.tolist()
 #df_clean = df.copy()
+
+df[["Numeric", "Low", "High"]] = df[["Numeric", "Low", "High"]] .astype("float")
 df.dtypes
 #missing_data = df.isnull()
 #missing_data.columns.values.tolist()
