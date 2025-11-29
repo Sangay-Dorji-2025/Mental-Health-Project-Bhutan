615712a0d15e7af19da9424d017b5913e64aecce
Sangay Dorji <sdorji753@gmail.com>
2025-11-29 17:08:05 +0600

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ df_clean = df.copy()
 
 df_clean.drop_duplicates(inplace=True)
 df_clean.fillna(0, inplace=True)
-df_clean[["Numeric", "Low", "High"]] = df_clean[["Numeric", "Low", "High"]].astype("float")
+#df_clean[["Numeric", "Low", "High"]] = df_clean[["Numeric", "Low", "High"]].astype("float")
 #df_clean.fillna(method="ffill", inplace=True)
 #df_clean.fillna(method="bfill", inplace=True)
 # -------------------------------------------------
