3db4edd3dbb381ffe2dd972329032237b8bc0a47
Sangay Dorji <sdorji753@gmail.com>
2025-11-30 13:58:04 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib
-matplotlib.use("Agg")  # Required for server environments
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 # import joblib 
 import os
