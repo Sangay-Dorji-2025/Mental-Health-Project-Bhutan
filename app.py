328094ed0a777b12bb3f2d19b93541184f6baf9a
Sangay Dorji <sdorji753@gmail.com>
2025-11-30 20:45:49 +0600

    Update app.py

--- app.py
+++ app.py
@@ -108,6 +108,7 @@ if st.checkbox("Show sample histogram"):
         ax.set_title(f"Histogram of {Bhutan Helat care}")
 
         st.pyplot(fig)
+        st.plt.show()
     else:
         st.warning("No numeric columns available.")
 
