ee864411a4724b147afad320dd42dcb3f1bf4bac
Sangay Dorji <sdorji753@gmail.com>
2025-11-30 11:49:13 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt
 # import joblib 
 import os
 st.set_page_config(page_title="Bhutan Healthcare Analytics", layout="wide")
