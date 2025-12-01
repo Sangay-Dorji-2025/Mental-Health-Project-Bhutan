8a0bcb40e2626530849f949dcecaf4d087187c9c
Sangay Dorji <sdorji753@gmail.com>
2025-12-01 11:57:09 +0600

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 # import joblib 
+import seaborn as sns
 import os
 st.set_page_config(page_title="Bhutan Healthcare Analytics", layout="wide")
 
