8b5c97f7ec050a067ad65d4b2e1cdc52b442be99
Sangay Dorji <sdorji753@gmail.com>
2025-12-03 09:28:29 +0600

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 # import joblib 
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from xgboost import XGBRegressor
 import os
 st.set_page_config(page_title="Bhutan Healthcare Analytics", layout="wide")
 
