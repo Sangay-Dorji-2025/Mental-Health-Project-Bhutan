176522f2b4b879f94c2b5104523f0a70ab65be5a
Sangay Dorji <sdorji753@gmail.com>
2025-12-04 15:29:13 +0600

    Update app.py

--- app.py
+++ app.py
@@ -77,9 +77,11 @@ df_clean = df.copy()
 indicator_list = sorted(df_clean["GHO (DISPLAY)"].unique())
 selected_indicator = st.selectbox("Choose an Indicator:", indicator_list)
 
-# Filter dataset
-df_clean = df.copy()
 df_filtered = df_clean[df_clean["GHO (DISPLAY)"] == selected_indicator].copy()
+
+# Remove the first row
+df_filtered = df_filtered.iloc[1:]
+
 st.write("### Filtered Data")
 st.dataframe(df_filtered)
 
