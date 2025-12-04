bf4bc91f8deb612c4a8189e4229fd3f1f57040df
Sangay Dorji <sdorji753@gmail.com>
2025-12-04 16:56:39 +0600

    Update app.py

--- app.py
+++ app.py
@@ -41,14 +41,6 @@ else:
 # SECTION 3: EXPLORATORY DATA ANALYSIS (EDIT AS NEEDED)
 # ----------------------------------------------------
 st.header("3. Exploratory Data Analysis (EDA)")
-#st.write("Add your own analyses here. Below are optional placeholders.")
-exclude_cols = ['YEAR (DISPLAY)', 'STARTYEAR', 'ENDYEAR']
-#numeric_cols = df_clean.select_dtypes(include='number').columns
-numeric_cols = df_clean.select_dtypes(include=[np.number]).columns.tolist()
-numeric_cols = [col for col in numeric_cols if col not in exclude_cols]
-if st.checkbox("Show summary statistics"):
-    st.write(df_clean[numeric_cols].describe())
-
 if st.checkbox("Show column info"):
     st.write(pd.DataFrame({
         "Column": df_clean.columns,
