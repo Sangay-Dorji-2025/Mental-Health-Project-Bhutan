83a81df56322ec6ad1594642b14da79a3cf8e351
Sangay Dorji <sdorji753@gmail.com>
2025-12-04 15:40:12 +0600

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ if mode == "Load Existing Model":
 else:
     st.title("Bhutan Health Care Data Science(Supervised Learning Model:  Linear Regression)")
     st.write(" ## Sample Dataset")
-    st.dataframe(df)
+    st.dataframe(df_filtered)
 
 # ---------------------------------------------------------
 # Build Linear Regression Model
