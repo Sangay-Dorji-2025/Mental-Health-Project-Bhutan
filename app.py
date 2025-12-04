b40bfdbb7df6b2e30c9902e81cfa2206b52d7e24
Sangay Dorji <sdorji753@gmail.com>
2025-12-04 15:33:00 +0600

    Update app.py

--- app.py
+++ app.py
@@ -73,14 +73,12 @@ st.dataframe(df_clean.head())
 # ----------------------------------------------------
 st.header("3. Exploratory Data Analysis (EDA)")
 #st.write("Add your own analyses here. Below are optional placeholders.")
-df_clean = df.copy()
+# Remove the first row
+df_filtered = df_filtered.iloc[1:]
 indicator_list = sorted(df_clean["GHO (DISPLAY)"].unique())
 selected_indicator = st.selectbox("Choose an Indicator:", indicator_list)
 
 df_filtered = df_clean[df_clean["GHO (DISPLAY)"] == selected_indicator].copy()
-
-# Remove the first row
-df_filtered = df_filtered.iloc[1:]
 
 st.write("### Filtered Data")
 st.dataframe(df_filtered)
